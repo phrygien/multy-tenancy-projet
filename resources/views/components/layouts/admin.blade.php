317979eab30e3e428a913b8b40a34f540dbb15eb
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-06-15 16:09:09 +0300

    logout admin tenancy

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -36,14 +36,7 @@
                     <x-menu-separator />
 
                     {{-- User --}}
-                @if($user = auth()->user())
-                        <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
-                            <x-slot:actions>
-                                <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
-                            </x-slot:actions>
-                        </x-list-item>
-                    @endif
-
+                    <livewire:admin.user-menu />
 
                     <x-menu-separator />
                 @endif
