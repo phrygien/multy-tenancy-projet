603781ee3aab5cab598d4375e82796cde8ece043
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-07-04 10:28:31 +0300

    crud promotion

--- app/Livewire/Tenant/Anneescolaire/AnneeList.php
+++ app/Livewire/Tenant/Anneescolaire/AnneeList.php
@@ -3,15 +3,52 @@
 namespace App\Livewire\Tenant\Anneescolaire;
 
 use App\Models\Anneescolaire;
+use App\Models\School;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
+use Livewire\Attributes\Validate;
 use Livewire\WithPagination;
+use Mary\Traits\Toast;
+use PhpParser\Node\Expr\FuncCall;
 
 class AnneeList extends Component
 {
     use WithPagination;
+    use Toast;
+
+    // for modal
+    public bool $anneeModal = false;
+    public bool $editMode = false;
+    public bool $deleteModal = false;
+    // properties
+    #[Validate('required')]
+    public string $name;
+
+    #[Validate('required')]
+    public $debut;
+
+    #[Validate('required')]
+    public $fin;
+
+    #[Validate('required')]
+    public $school_id;
+
+    public $annee_id;
+
+    public function showModal(): void
+    {
+        $this->reset();
+        $this->anneeModal = true;
+    }
+
+    public function mount(): void
+    {
+
+    }
+
     public function render()
     {
+        $ecoles = School::all();
         $anneescolaires = DB::table('anneescolaires')
         ->join('schools', 'schools.id', '=', 'anneescolaires.school_id')
         ->select('anneescolaires.*', 'schools.school_name')
@@ -28,7 +65,67 @@ class AnneeList extends Component
 
         return view('livewire.tenant.anneescolaire.annee-list', [
             'headers' => $headers,
-            'anneescolaires' => $anneescolaires
+            'anneescolaires' => $anneescolaires,
+            'ecoles' => $ecoles
         ]);
+    }
+
+    // save promotion
+    public function save(): void
+    {
+        $this->validate();
+        try {
+            if($this->editMode)
+            {
+                $this->validate();
+                $annee = Anneescolaire::find($this->annee_id);
+
+                $annee->school_id = $this->school_id;
+                $annee->name = $this->name;
+                $annee->debut = $this->debut;
+                $annee->fin = $this->fin;
+                $annee->save();
+                $this->editMode = false;
+                $this->success('Mise a jour bien enregistré');
+                $this->reset();
+            }else{
+                Anneescolaire::create([
+                    'name' => $this->name,
+                    'debut' => $this->debut,
+                    'fin' => $this->fin,
+                    'school_id' => $this->school_id
+                ]);
+                $this->success('Promotion bien enregistré');
+                $this->reset();
+            }
+        } catch (\Exception $e) {
+            $this->error('Promotion not saved !');
+        }
+    }
+
+    public function edit($id): void
+    {
+        $annee = Anneescolaire::find($id);
+        $this->annee_id = $annee->id;
+        $this->name = $annee->name;
+        $this->debut = $annee->debut;
+        $this->fin = $annee->fin;
+        $this->school_id = $annee->school_id;
+        $this->editMode = true;
+        $this->anneeModal = true;
+    }
+
+    public function confirmerDelete($id): void
+    {
+        $this->deleteModal = true;
+        $annee = Anneescolaire::find($id);
+        $this->annee_id = $annee->id;
+    }
+    public function delete(): void
+    {
+        Anneescolaire::find($this->annee_id)->delete();
+        $this->success('Suppression bien enregistré');
+        $this->deleteModal = false;
+        $this->reset();
     }
 }
--- resources/views/livewire/tenant/anneescolaire/annee-list.blade.php
+++ resources/views/livewire/tenant/anneescolaire/annee-list.blade.php
@@ -2,12 +2,48 @@
     <x-header title="Année scolaire" subtitle="Gerer l'année scolaire pour la promotion">
         <x-slot:actions>
             <x-button icon="o-funnel" />
-            <x-button icon="o-plus" class="btn-primary" label="Ajouter année scolaire" />
+            <x-button icon="o-plus" class="btn-primary" label="Créer une année" @click="$wire.showModal()" />
         </x-slot:actions>
     </x-header>
 
     <x-card separator progress-indicator>
-        <x-table :headers="$headers" :rows="$anneescolaires" @row-click="alert($event.detail.name)" />
+        <x-table :headers="$headers" :rows="$anneescolaires" @row-click="$wire.edit($event.detail.id)">
+            @scope('actions', $annee)
+                <x-button icon="o-trash" wire:click="confirmerDelete({{ $annee->id }})" spinner class="btn-sm" />
+            @endscope
+        </x-table>
+
     </x-card>
+
+    <x-mary-modal wire:model="anneeModal" class="backdrop-blur" title="Ajout Annee">
+        <div class="mb-5">
+            <x-form wire:submit="save">
+                {{-- Notice `error_field` --}}
+                <x-select
+                label="Alternative"
+                :options="$ecoles"
+                option-value="id"
+                option-label="school_name"
+                placeholder="Ecole ?"
+                placeholder-value="0"
+                hint=""
+                wire:model.live="school_id" />
+
+                <x-mary-input label="Nom de l'annee (promotion)" wire:model="name" />
+                <x-datetime label="Date debut" wire:model="debut" icon="o-calendar" />
+                <x-datetime label="Date fin" wire:model="fin" icon="o-calendar" />
+                <x-slot:actions>
+                    <x-mary-button label="Annuler" @click="$wire.anneeModal = false" />
+                    <x-mary-button label="Valider" class="btn-primary" type="submit" spinner="save" icon="o-paper-airplane" />
+                </x-slot:actions>
+            </x-form>
+        </div>
+    </x-mary-modal>
+
+    <x-modal wire:model="deleteModal" class="backdrop-blur">
+        <div class="mb-5">Cliquer sur `Confirmer` pour supprimer, ou `ANNULER` pour annuler la suppression.</div>
+        <x-button label="Annuler" @click="$wire.deleteModal = false" />
+        <x-button label="Confirmer" wire:click='delete()' class="btn-error" />
+    </x-modal>
 
 </div>
