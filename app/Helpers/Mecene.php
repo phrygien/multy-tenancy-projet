8659f91c2159ca06e17314db00618f13bfd9f38d
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-07-06 13:47:53 +0300

    check limit student

--- app/Helpers/Mecene.php
+++ app/Helpers/Mecene.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\School;
 use App\Models\Pricing;
 use App\Models\Abonnement;
+use App\Models\Eleve;
 
 class Mecene
 {
@@ -39,5 +40,35 @@ class Mecene
         }
 
         return ['status' => false];
+    }
+
+    // ckeck limit eleve
+    public static function hasReachedStudentLimit()
+    {
+        try {
+            $user = Auth::user();
+            $school = School::where('user_id', $user->id)->first();
+
+            $abonnement = Abonnement::where('user_id', $user->id)
+            ->where('tenant_id', $user->tenant->id)
+            ->where('is_active', 1)
+            ->first();
+
+             if(!$school) {
+                return ['status' => false, 'message' => 'Vous n\'etes pas encore affectez a un ecole !'];
+             }
+
+             $pricing = Pricing::where('id', $abonnement->pricing_id)->first();
+            $currentStudentCount = Eleve::where('school_id', $school->id)->count();
+
+            if ($currentStudentCount >= $pricing->max_student) {
+                return ['status' => true, 'message' => 'Vous avez atteint le nombre maximum d\'eleves autorisés'];
+            }
+
+            return ['status' => false];
+
+        } catch (\Exception $e) {
+            dd($e);
+        }
     }
 }
